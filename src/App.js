34b38b97ff2ac91c7b750b77018c0cdfdfa25296
LisinIwan <LisinIwan@gmx.de>
2021-07-25 03:56:58 +0200

    newme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
           <Route exact path='/' component={HomePage} />
       </Switch>
