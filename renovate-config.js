9b818cd4db5a65c77641fe23f456b69e52410364
LisinIwan <LisinIwan@gmx.de>
2022-09-19 07:19:15 +0200

    Create renovate-config.js

---
+++ renovate-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  branchPrefix: 'test-renovate/',
+  dryRun: true,
+  username: 'renovate-release',
+  gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
+  onboarding: false,
+  platform: 'github',
+  includeForks: true,
+  repositories: [
+    'renovatebot/github-action',
+    'renovate-tests/cocoapods1',
+    'renovate-tests/gomod1',
+    'LisinIwan/Test-App',
+  ],
+  packageRules: [
+    {
+      description: 'lockFileMaintenance',
+      matchUpdateTypes: [
+        'pin',
+        'digest',
+        'patch',
+        'minor',
+        'major',
+        'lockFileMaintenance',
+      ],
+      dependencyDashboardApproval: false,
+      stabilityDays: 0,
+    },
+  ],
+};
